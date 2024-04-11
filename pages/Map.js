f642e8075ce06835ecef274b618b40cd5426eaca
PRAMOD222 <pramodkesarkar2222@gmail.com>
2024-04-11 13:02:15 +0530

    done

--- pages/Map.js
+++ pages/Map.js
@@ -1,15 +1,9 @@
 import Head from 'next/head';
-import RealTimeTracker from '../components/firebaseConfig';
 
 const MyPage = () => (
-  <>
-    <Head>
-    {/* <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyBIHv7Vci9zEHGqW9g244wdwHUFdvfC2Ds&callback=initialize"></script> */}
-    </Head>
     <div>
-      <RealTimeTracker />
+      map page
     </div>
-  </>
 );
 
 export default MyPage;
