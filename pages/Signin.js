08b044f9ab1caeee409606def1e8d1ffe9d8458a
PRAMOD222 <pramodkesarkar2222@gmail.com>
2024-04-11 14:24:05 +0530

    url fix for the signin and login api

--- pages/Signin.js
+++ pages/Signin.js
@@ -29,7 +29,7 @@ const Signin = () => {
     e.preventDefault();
     const data = { name, email, password }
 
-    let res = await fetch('http://localhost:3000/api/signin', {
+    let res = await fetch('/api/signin', {
       method: 'POST', // or 'PUT'
       headers: {
         'Content-Type': 'application/json',
